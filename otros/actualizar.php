5481aed8086d42578975535b5411c456db7aa8ee
ne-jcruiz <juanbulls@yahoo.com>
2024-05-02 23:00:04 -0500

    Update actualizar.php

--- otros/actualizar.php
+++ otros/actualizar.php
@@ -1,6 +1,20 @@
 <?php
 
-shell_exec('cd /var/www/html && git pull');
-echo "Solicitud recibida. \n";
+// Validate the request is coming from GitHub
+$signature = $_SERVER['HTTP_X_HUB_SIGNATURE'] ?? '';
+$payload = file_get_contents('php://input');
+if (!verify_signature($payload, $signature)) {
+    header('HTTP/1.0 403 Forbidden');
+    die("Firma invalida.");
+}
 
-?>
+// Execute git pull
+$output = shell_exec('cd /var/www/html && git pull');
+echo "Pull Ok. \n";
+
+// Verify the signature
+function verify_signature($payload, $signature) {
+    $secret = 'rappiMeLaPela';
+    $hash = "sha1=" . hash_hmac('sha1', $payload, $secret);
+    return hash_equals($hash, $signature);
+}
