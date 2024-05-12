bd77faac788a68f83e1acd67ca80f7f69be33265
ne-jcruiz <juanbulls@yahoo.com>
2024-05-11 20:06:42 -0500

    Funcion Limpiar

--- final.js
+++ final.js
@@ -1,1 +1,8 @@
 document.getElementById("pj").innerHTML = "En contrucción";
+
+function limpiarNuevaData(){
+    var inputs = document.querySelectorAll('.nuevaData input');
+    inputs.forEach(function(input) {
+        input.value = '';
+    });
+}
