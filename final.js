9d08780e844519a9bde65fd0f5a31ea34352a7fc
ne-jcruiz <juanbulls@yahoo.com>
2024-05-11 20:39:08 -0500

    Estilos para el td de un imput focusado

--- final.js
+++ final.js
@@ -6,3 +6,17 @@ function limpiarNuevaData(){
         input.value = '';
     });
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    var inputs = document.querySelectorAll('.nuevaData input[type="text"]');
+    inputs.forEach(function(input) {
+        input.addEventListener('focus', function() {
+            var td = this.parentElement;
+            td.style.borderBottom = '2px solid rgb(78, 58, 255)';
+        });
+        input.addEventListener('blur', function() {
+            var td = this.parentElement;
+            td.style.borderBottomColor  = ''; // Reset border color
+        });
+    });
+});
