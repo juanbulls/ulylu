54b793170232d9cfef5607491c853723b17f2f05
ne-jcruiz <juanbulls@yahoo.com>
2024-05-29 20:26:57 -0500

    Filtrado de data

--- phps/data.php
+++ phps/data.php
@@ -14,7 +14,13 @@ while ($row = mysqli_fetch_assoc($cols_result)) {
 $data_result = q("SELECT * FROM $base.$tabla");
 $data = [];
 while ($row = mysqli_fetch_assoc($data_result)) {
-    $data[] = $row;
+    $filtered_row = [];
+    foreach ($cols as $col) {
+        if (isset($row[$col])) {
+            $filtered_row[$col] = $row[$col];
+        }
+    }
+    $data[] = $filtered_row;
 }
 
 $response = [
