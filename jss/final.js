aacef7875ebecf5d64f572a922d4231383694a4d
ne-jcruiz <juanbulls@yahoo.com>
2024-06-01 20:59:24 -0500

    Populador

--- jss/final.js
+++ jss/final.js
@@ -14,14 +14,49 @@ function limpiarNuevaData(){
         input.value = '';
     });
 }
-// TODO:
-// inputsDataNueva <td><input type="[]" placeholder="columna"...
-// headerDataVieja <th...
-// dataVieja <tr><td data-cell="[columna]">[valor]...
+
+function esFecha(cadena) {
+    // Check if the format is YYYY-MM-DD using a regular expression
+    const regex = /^\d{4}-\d{2}-\d{2}$/;
+    return regex.test(cadena);
+}
 
 // Populador
-function datearGrilla(jsonData) {
-    console.log(jsonData);
+let jsonData = [];
+function datearGrilla(d) {
+    // Nueva Data y Headers registros
+    d.cols.forEach((col) => {
+        let tipo = 'text';
+        if (esFecha(d.data[0][col])) {
+            tipo = 'date';
+        }
+
+        const ntd = document.createElement('td');
+        const input = document.createElement('input');
+        input.type = tipo;
+        input.placeholder = col;
+
+        ntd.appendChild(input);
+        id('inputsDataNueva').appendChild(ntd); // Nueva Data
+
+        const th = document.createElement('th');
+        th.innerHTML = col;
+        id('headerDataVieja').appendChild(th); // Encabezado
+    });
+    
+    // Registros historia
+    d.data.forEach((f) => {
+        const tr = document.createElement('tr');
+        d.cols.forEach((c) => {
+            const td = document.createElement('td');
+            td.setAttribute('data-cell', c);
+            td.innerHTML = f[c];
+
+            tr.appendChild(td);
+        });
+
+        id('dataVieja').appendChild(tr);
+    });
 }
 
 // Ejecuciones
