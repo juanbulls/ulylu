2d9bf1a81f058bb76f27fd645591045b6e887bee
ne-jcruiz <juanbulls@yahoo.com>
2024-07-06 22:49:48 -0500

    Update registrar.php

--- phps/registrar.php
+++ phps/registrar.php
@@ -11,6 +11,20 @@ while ($row = mysqli_fetch_assoc($cols_result)) {
     }
 }
 
+// Mockeando
+$_REQUEST = [
+    "base" => "your_base_value",
+    "tabla" => "your_tabla_value",
+    "Cliente_r" => "Ruiz",
+    "Vendedor_r" => "Rodriguez",
+    "Fecha_d" => "2024-07-11",
+    "nTdoc" => "dasd",
+    "nNdoc" => "1234",
+    "nCuenta" => "U",
+    "nCantidad" => "1",
+    "nDescripcion" => "fdgsdfg",
+    "nComentario" => "sdfgsdfg"
+];
 $insert_data = [];
 foreach ($_REQUEST as $key => $value) {
     if (!in_array($key, ['base', 'tabla'])) {
@@ -18,27 +32,5 @@ foreach ($_REQUEST as $key => $value) {
     }
 }
 
-$fields = implode(",", array_keys($insert_data));
-$values = implode("','", array_map('mysqli_real_escape_string', array_values($insert_data)));
-$insert_result = q("INSERT INTO $base.$tabla ($fields) VALUES ('$values')");
-
-if (!$insert_result) {
-    $response = [
-        "error" => "error inserting"
-    ];
-} else {
-    $data_result = q("SELECT * FROM $base.$tabla ORDER BY id DESC LIMIT 1");
-    $data = [];
-    while ($row = mysqli_fetch_assoc($data_result)) {
-        $data[] = $row;
-    }
-
-    $response = [
-        "cols" => $cols,
-        "data" => $data
-    ];
-}
-
-header('Content-Type: application/json');
-echo json_encode($response);
+echo json_encode($insert_data);
 ?>
