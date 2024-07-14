0356e18b0d3c606d005403dfbdc2a11273f34dbb
ne-jcruiz <juanbulls@yahoo.com>
2024-07-13 19:22:29 -0500

    Buscador semidinamico

--- csss/estilos.js
+++ csss/estilos.js
@@ -25,3 +25,40 @@ const spinner = {
         id('subData').style.display = 'none';
     },
 }
+
+// Icono Buscador
+const rect = id('buscRect');
+const line = id('buscLine');
+const desplazoX = buscador.clientWidth - 30;
+
+rect.setAttribute("x", desplazoX + 3);
+rect.setAttribute('width', 23);
+
+line.setAttribute("x1", desplazoX + 30);
+line.setAttribute("x2", desplazoX + 23);
+
+id('buscador').addEventListener('click', toggleBuscador);
+
+let valToggle = false;
+function toggleBuscador() {
+    if (valToggle) { // Colapsado
+        rect.setAttribute("x", desplazoX + 3);
+        rect.setAttribute('width', 23);
+        rect.setAttribute("rx", 23);
+        rect.setAttribute("ry", 23);
+        rect.setAttribute("stroke-width", 3);
+        
+        line.setAttribute("x1", desplazoX + 30);
+        line.setAttribute("x2", desplazoX + 23);
+    } else { // Expandido
+        rect.setAttribute("x", 3);
+        rect.setAttribute('width', desplazoX + 23);
+        rect.setAttribute("rx", 2);
+        rect.setAttribute("ry", 2);
+        rect.setAttribute("stroke-width", 1);
+        
+        line.setAttribute("x1", 30);
+        line.setAttribute("x2", 23);
+    }
+    valToggle = !valToggle
+}
