dacc0421e305593ede548794ac4f4eb7061690ef
ne-jcruiz <juanbulls@yahoo.com>
2024-07-28 10:24:33 -0500

    prueba creador relacionadas

--- phps/registrar.php
+++ phps/registrar.php
@@ -12,12 +12,12 @@ while ($row = mysqli_fetch_assoc($cols_result)) {
 }
 
 // Mockeando
-/*
+
 $_REQUEST = [
     "base" => "your_base_value",
     "tabla" => "your_tabla_value",
-    "Cliente_r" => "Ruiz",
-    "Vendedor_r" => "Rodriguez",
+    "Cliente_r" => "David Mendoza",
+    "Vendedor_r" => "Javier Murillo",
     "Fecha_d" => "2024-07-11",
     "Ndoc" => "1234",
     "Tdoc" => "dasd",
@@ -26,11 +26,27 @@ $_REQUEST = [
     "Descripcion" => "prueba error handeling",
     "Comentario" => "prueba numero 4"
 ];
-*/
+
 $insert_data = [];
+$related_entities = [];
+
 foreach ($_REQUEST as $key => $value) {
     if (!in_array($key, ['base', 'tabla'])) {
-        $insert_data[$key] = $value;
+        if(substr($key, -1) == '_r'){
+            $entity_table = strtolower(substr($key, 0, -2));
+            $entity_name = mysqli_real_escape_string($con, $value);
+
+            // Revisar si existe
+            $entity_result = q("SELECT * FROM $base.$entity_table WHERE Nombre = '$entity_name';");
+            if(mysqli_num_rows($entity_result) == 0) {
+                $insert = q("INSERT INTO $base.$entity_table (Nombre) VALUES ('$entity_name');");
+                if (!$insert) {
+                    throw new Exception("Error insertando entidad: $entity_table con valor: $entity_name");
+                }
+            }
+        } else {
+            $insert_data[$key] = $value;
+        }
     }
 }
 
@@ -38,6 +54,7 @@ $fields = implode(",", array_keys($insert_data));
 $values = implode("','", array_map(function($value) use ($con) {
     return mysqli_real_escape_string($con, $value);
 }, array_values($insert_data)));
+
 try {
     $insert_result = q("INSERT INTO $base.$tabla ($fields) VALUES ('$values')");
 
