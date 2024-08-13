bfb2e0444ae8f1e228edf8269f77500de47892d4
ne-jcruiz <juanbulls@yahoo.com>
2024-08-13 09:06:10 -0500

    Update subData.php

--- phps/subData.php
+++ phps/subData.php
@@ -5,7 +5,7 @@ $base = isset($_REQUEST["base"]) ? $_REQUEST["base"] : (isset($argv[1]) ? $argv[
 $tabla = isset($_REQUEST["tabla"]) ? $_REQUEST["tabla"] : (isset($argv[2]) ? $argv[2] : null);
 $patron = isset($_REQUEST["patron"]) ? $_REQUEST["patron"] : (isset($argv[3]) ? $argv[3] : null);
 
-$data_result = q("SELECT Nombre FROM $base.$tabla WHERE Nombre LIKE '%$patron%' LIMIT 5;");
+$data_result = q("SELECT Nombre FROM $base.$tabla WHERE Nombre LIKE '%$patron%' ORDER BY Nombre LIMIT 5;");
 $data = [];
 while ($row = mysqli_fetch_assoc($data_result)) {
     $data[] = $row['Nombre'];
