d4931e903142b18175c2f13473af5478a32db888
ne-jcruiz <juanbulls@yahoo.com>
2024-08-27 21:09:30 -0500

    Update final.js

--- jss/final.js
+++ jss/final.js
@@ -108,7 +108,7 @@ function datearRegistros(d) {
 
         const limite = 90;
         if(textoResumen.length > limite) {
-            id('resumen').setAttribute('title', textoResumen.replace('<b>', '').replace('</b>', ''));
+            id('resumen').setAttribute('title', textoResumen.replace(new RegExp('<b>', 'g'), '').replace(new RegExp('</b>', 'g'), ''));
             textoResumen = textoResumen.slice(0, limite) + '...';
         }
         id('resumen').innerHTML = textoResumen;
