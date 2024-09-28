e4592797672f5a8c85e903b4419eaada5d2af171
ne-jcruiz <juanbulls@yahoo.com>
2024-09-28 12:34:28 -0500

    Update pruebaHash.php

--- phps/pruebaHash.php
+++ phps/pruebaHash.php
@@ -1,7 +1,9 @@
 <?php
 
 $pass = isset($_REQUEST["pass"]) ? $_REQUEST["pass"] : (isset($argv[1]) ? $argv[1] : null);
+$hash = isset($_REQUEST["hash"]) ? $_REQUEST["hash"] : (isset($argv[2]) ? $argv[2] : null);
 
-echo password_hash($pass, PASSWORD_DEFAULT);
+// echo password_hash($pass, PASSWORD_DEFAULT);
+echo password_verify($pass, $hash);
 
 ?>
