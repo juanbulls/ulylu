37c462e421ed419fbb52917e4be2a370e023c12b
ne-jcruiz <juanbulls@yahoo.com>
2024-09-28 20:40:32 -0500

    Update validacion.php

--- phps/validacion.php
+++ phps/validacion.php
@@ -2,21 +2,21 @@
 include('marlene.php');
 
 $base = isset($_REQUEST["base"]) ? $_REQUEST["base"] : (isset($argv[1]) ? $argv[1] : null);
-$usr = isset($_REQUEST["usr"]) ? $_REQUEST["usr"] : (isset($argv[2]) ? $argv[2] : null);
-$token = isset($_REQUEST["token"]) ? $_REQUEST["token"] : (isset($argv[3]) ? $argv[3] : null);
+$token = isset($_REQUEST["token"]) ? $_REQUEST["token"] : (isset($argv[2]) ? $argv[2] : null);
 
-$data_result = q("SELECT * FROM $base.uszAccesos WHERE hash='$token' AND email='$usr';");
+$data_result = q("SELECT * FROM $base.uszAccesos WHERE hash='$token';");
 
 if (mysqli_num_rows($data_result) == 1) {
     $row = mysqli_fetch_assoc($data_result);
     $modificado = new DateTime($row['modificado']);
+    $email = $row['email'];
     $hace3meses = new DateTime();
     $hace3meses->modify('-90 days');
     if ($modificado < $hace3meses) {
         $response = [ "error" => "vencido" ];
     } else {
-        $actualizar = q("UPDATE $base.uszAccesos SET email = '$usr';");
-        $response = [ "validado" => "ok" ];
+        $actualizar = q("UPDATE $base.uszAccesos SET hash = '$token';");
+        $response = [ "email" => $email ];
     }
 } else {
     $response = [ "error" => "invalidado" ];
