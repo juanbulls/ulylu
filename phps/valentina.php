daa7a58d3c29c07b877325c56dd2df68b9426875
ne-jcruiz <juanbulls@yahoo.com>
2024-10-02 18:30:26 -0500

    Update valentina.php

--- phps/valentina.php
+++ phps/valentina.php
@@ -4,7 +4,8 @@ function v($base) {
     $token = isset($_REQUEST["token"]) ? $_REQUEST["token"] : (isset($argv[1]) ? end($argv) : null);
     
     $data_result = q("SELECT * FROM $base.uszAccesos WHERE hash='$token';");
-
+    echo "SELECT * FROM $base.uszAccesos WHERE hash='$token';";
+    echo "argo".mysqli_num_rows($data_result);
     if (is_null($token) || !mysqli_num_rows($data_result) == 1) {
         echo json_encode(["error" => "invalidado"]);
         exit;
