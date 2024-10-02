be8f032cb6f65799698da1bdd9706cd87fe89c3a
ne-jcruiz <juanbulls@yahoo.com>
2024-10-02 18:47:09 -0500

    Update valentina.php

--- phps/valentina.php
+++ phps/valentina.php
@@ -4,7 +4,7 @@ function v($base) {
     $token = isset($_REQUEST["token"]) ? $_REQUEST["token"] : null;
 
     $data_result = q("SELECT * FROM $base.uszAccesos WHERE hash='$token';");
-    echo "SELECT * FROM $base.uszAccesos WHERE hash='$token';";
+    
     echo "contador: ". count($argv);
     if (is_null($token) || !mysqli_num_rows($data_result) == 1) {
         echo json_encode(["error" => "invalidado"]);
