38fada62e722991edc4e941348febbba91dfde04
ne-jcruiz <juanbulls@yahoo.com>
2024-10-02 18:59:55 -0500

    Update valentina.php

--- phps/valentina.php
+++ phps/valentina.php
@@ -4,15 +4,15 @@ include('marlene.php');
 $token = isset($_REQUEST["token"]) ? $_REQUEST["token"] : (isset($argv[1]) ? $argv[1] : null);
 
 echo "token: ".is_null($token);
-/*
+
 function v($base) {
     $data_result = q("SELECT * FROM $base.uszAccesos WHERE hash='$token';");
 
     echo "contador: ". is_null($token);
-    if (is_null($token) || !mysqli_num_rows($data_result) == 1) {
+    if (is_null($token)/* || !mysqli_num_rows($data_result) == 1*/) {
         echo json_encode(["error" => "invalidado"]);
         exit;
     }
 }
-v('bauer');*/
+v('bauer');
 ?>
