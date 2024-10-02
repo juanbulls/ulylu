8d05d54d1d4963417f22e1c227806818f30fce75
ne-jcruiz <juanbulls@yahoo.com>
2024-10-02 18:21:59 -0500

    Create valentina.php

---
+++ phps/valentina.php
@@ -0,0 +1,13 @@
+<?php
+include('marlene.php');
+function v() {
+    $token = isset($_REQUEST["token"]) ? $_REQUEST["token"] : (isset($argv[1]) ? end($argv) : null);
+    
+    $data_result = q("SELECT * FROM $base.uszAccesos WHERE hash='$token';");
+
+    if (is_null($token) || !mysqli_num_rows($data_result) == 1) {
+        echo json_encode(["error" => "invalidado"]);
+        exit;
+    }
+}
+?>
