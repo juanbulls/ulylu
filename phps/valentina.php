d3ae5fe41a637fc1d0bbc6d2339013a62fbd2ba5
ne-jcruiz <juanbulls@yahoo.com>
2024-10-02 19:01:32 -0500

    Update valentina.php

--- phps/valentina.php
+++ phps/valentina.php
@@ -1,11 +1,8 @@
 <?php
 include('marlene.php');
 
-$token = isset($_REQUEST["token"]) ? $_REQUEST["token"] : (isset($argv[1]) ? $argv[1] : null);
-
-echo "token: ".is_null($token);
-
 function v($base) {
+    $token = isset($_REQUEST["token"]) ? $_REQUEST["token"] : (isset($argv[1]) ? $argv[1] : null);
     $data_result = q("SELECT * FROM $base.uszAccesos WHERE hash='$token';");
 
     echo "contador: ". is_null($token);
