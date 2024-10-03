a44ab6a74df7e519a4813e4a8c3adf3e1fccead6
ne-jcruiz <juanbulls@yahoo.com>
2024-10-02 20:15:32 -0500

    Update subData.php

--- phps/subData.php
+++ phps/subData.php
@@ -1,9 +1,12 @@
 <?php
 include('marlene.php');
+include('valentina.php');
 
 $base = isset($_REQUEST["base"]) ? $_REQUEST["base"] : (isset($argv[1]) ? $argv[1] : null);
 $tabla = isset($_REQUEST["tabla"]) ? $_REQUEST["tabla"] : (isset($argv[2]) ? $argv[2] : null);
 $patron = isset($_REQUEST["patron"]) ? $_REQUEST["patron"] : (isset($argv[3]) ? $argv[3] : null);
+
+v($base);
 
 $data_result = q("SELECT Nombre FROM $base.$tabla WHERE Nombre LIKE '%$patron%' ORDER BY Nombre LIMIT 5;");
 $data = [];
