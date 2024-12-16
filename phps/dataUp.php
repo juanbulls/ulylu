796be59c15177cbff436354ca100ee7a4682275b
Juan Ruiz <juanbulls@yahoo.com>
2024-12-15 21:40:08 -0500

    Update dataUp.php

--- phps/dataUp.php
+++ phps/dataUp.php
@@ -1,6 +1,6 @@
 <?php
 include('marlene.php');
-// include('valentina.php');
+include('valentina.php');
 
 $base = isset($_REQUEST["base"]) ? $_REQUEST["base"] : (isset($argv[1]) ? $argv[1] : null);
 $tab = isset($_REQUEST["tab"]) ? $_REQUEST["tab"] : (isset($argv[2]) ? $argv[2] : null);
@@ -8,11 +8,14 @@ $col = isset($_REQUEST["col"]) ? $_REQUEST["col"] : (isset($argv[3]) ? $argv[3] 
 $reg = isset($_REQUEST["reg"]) ? $_REQUEST["reg"] : (isset($argv[4]) ? $argv[4] : null);
 $val = isset($_REQUEST["val"]) ? $_REQUEST["val"] : (isset($argv[5]) ? $argv[5] : null);
 
-// v($base);
+v($base);
                 
 try {
-    // echo "UPDATE $base.`$tab` SET `$col` = '$val' WHERE id = '$reg';";
-    $insert_result = q("UPDATE $base.`$tab` SET `$col` ='$val' WHERE id = '$reg';");
+    if ( is_null($val) ){
+        $insert_result = q("UPDATE $base.`$tab` SET `$col` = NULL WHERE id = '$reg';");
+    } else {
+        $insert_result = q("UPDATE $base.`$tab` SET `$col` = '$val' WHERE id = '$reg';");
+    }
 
     $response = [
         "estado" => 'actualizado'
