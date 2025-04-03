0ab7e92b391e9fb7d9d2f87da84ffd9e8ad2fd77
Juan Ruiz <juanbulls@yahoo.com>
2025-04-03 18:30:33 -0500

    Puerta

--- jss/final.js
+++ jss/final.js
@@ -398,7 +398,7 @@ if (!esLocal) {
                 document.cookie = galleta('token', token, 90); // 90 dias, 3 meses +/-
                 document.cookie = galleta('user', r.email, 90);
                 user = r.email;
-                id('usuario').innerHTML = pascalCase(user) + ' ( <a href="javascript:void(0);" onclick="deslogear()">salir</a> )';
+                id('usuario').innerHTML = pascalCase(user) + '<a href="javascript:void(0);" onclick="deslogear()">🚪</a> ';
 
                 cargaInicial();
             } else {
