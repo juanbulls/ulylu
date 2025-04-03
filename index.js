d3599c1ac6e7c983d0bc4ade96ed8b9d69cef2ef
Juan Ruiz <juanbulls@yahoo.com>
2025-04-03 18:14:39 -0500

    Carga dinamica de scripts

---
+++ index.js
@@ -0,0 +1,15 @@
+const scripts = [
+    "csss/estilos.js",
+    "jss/localJson.js",
+    "jss/el.js",
+    "jss/final.js"
+];
+
+scripts.reduce((p, src) =>
+    p.then(() => new Promise(res => {
+        let s = document.createElement("script");
+        s.src = src;
+        s.onload = res;
+        document.body.appendChild(s);
+    }
+)), Promise.resolve());
