a97a5124240aeb3b60c4700f394ce412cd71a052
Juan Ruiz <juanbulls@yahoo.com>
2025-04-18 17:39:02 -0500

    infinit scroll populador

--- jss/servicios/util.js
+++ jss/servicios/util.js
@@ -3,7 +3,13 @@ let cargando = false;
 window.addEventListener('scroll', () => {
     if ((window.innerHeight + window.scrollY) >= document.documentElement.scrollHeight - 100) {
         if (!cargando) {
-            alert("llego abajo");
+            cargando = true;
+            gOffset ++;
+            pedirData('data', `base=${bdBase}&tabla=${bdTabla}`, null, gOffset).then(r => {
+                datearRegistros(r);
+                dataSpinner.ocultar();
+                cargando = false;
+            });
         }
     }
 });
