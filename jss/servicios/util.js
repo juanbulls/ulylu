5107d7560ebaab49564080de8a321c5e5e393b52
Juan Ruiz <juanbulls@yahoo.com>
2025-04-18 20:30:39 -0500

    Mensajes mientras carga la data

--- jss/servicios/util.js
+++ jss/servicios/util.js
@@ -4,10 +4,18 @@ window.addEventListener('scroll', () => {
     if ((window.innerHeight + window.scrollY) >= document.documentElement.scrollHeight - 100) {
         if (!scrolleando) {
             scrolleando = true;
+            id("final").innerHTML = "Cargando siguiente data...";
+
             const patron = id('buscTexto').value != '' ? id('buscTexto').value : null;
             gOffset ++;
+
             pedirData('data', `base=${bdBase}&tabla=${bdTabla}`, patron, gOffset).then(r => {
-                datearRegistros(r);
+                if (r.data.length === 0) {
+                    id("final").innerHTML = "Fin de la data.";
+                } else {
+                    datearRegistros(r);
+                    id("final").innerHTML = "";
+                }
                 dataSpinner.ocultar();
                 scrolleando = false;
             });
