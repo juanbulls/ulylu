e3a42126b28f7d1808fd950ae5cd5ce9fb41aa62
Juan Ruiz <juanbulls@yahoo.com>
2025-04-18 20:13:40 -0500

    Update util.js

--- jss/servicios/util.js
+++ jss/servicios/util.js
@@ -4,8 +4,9 @@ window.addEventListener('scroll', () => {
     if ((window.innerHeight + window.scrollY) >= document.documentElement.scrollHeight - 100) {
         if (!cargando) {
             cargando = true;
+            const patron = id('buscTexto').value != '' ? id('buscTexto').value : null;
             gOffset ++;
-            pedirData('data', `base=${bdBase}&tabla=${bdTabla}`, null, gOffset).then(r => {
+            pedirData('data', `base=${bdBase}&tabla=${bdTabla}`, patron, gOffset).then(r => {
                 datearRegistros(r);
                 dataSpinner.ocultar();
                 cargando = false;
