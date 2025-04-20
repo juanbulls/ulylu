3d7db51db10672c82724b0b243c688f9be7ee1de
Juan Ruiz <juanbulls@yahoo.com>
2025-04-20 10:14:37 -0500

    Update csv.php

--- phps/csv.php
+++ phps/csv.php
@@ -9,23 +9,16 @@ $tabla = isset($_REQUEST["tabla"]) ? $_REQUEST["tabla"] : (isset($argv[2]) ? $ar
 v($base); // Descomentar cuando termine debugg
 
 $cols_result = q("SHOW COLUMNS FROM $base.$tabla");
-$cols = [];
 while ($row = mysqli_fetch_assoc($cols_result)) {
     if (ctype_upper($row['Field'][0])) {
-        $cols[] = $row['Field'];
+        $response[] = $row['Field'];
     }
 }
 
 $data_result = q("SELECT * FROM $base.$tabla;");
-$data = [];
 while ($row = mysqli_fetch_assoc($data_result)) {
-    $data[] = $row;
+    $response[] = $row;
 }
-
-$response = [
-    "cols" => $cols,
-    "data" => $data
-];
 
 header('Content-Type: application/json');
 echo json_encode($response);
